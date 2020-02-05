3354df92208cbacabd34f3b3809a5f017a08dcae
silmasis <silmasis@gmail.com>
2020-02-04 20:58:11 -0500

    added the yahoo api

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,125 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+$("#select-stock").on("click", function (event) {
+  // Preventing the button from trying to submit the form
   event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
+  // Storing the stock name
+  var inputStock = $("#stock-input").val().trim().toUpperCase()
+  // Running the searchStocks function(passing in the symbol as an argument)
+  searchStocks(inputStock);
+  getOptions(inputStock);
+  console.log(inputStock);
+});
+function searchStocks(inputStock) {
+  //Yahoo Finance Api
+  var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-detail" + "?region=US&lang=en&symbol=" + inputStock,
+      "method": "GET",
+      "headers": {
+          "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+          "x-rapidapi-key": "ea9381a499msh1c1d7c0f469c737p179f05jsnd2f7d7ffd270"
+      }
   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
+  $.ajax(settings).done(function (response) {
+      console.log(response.quoteData[inputStock]);
+
+      var symbolName = $("<h1>").text(response.quoteData[inputStock].longName);
+      var symbolPrice = $("<h2>").text(response.quoteData[inputStock].regularMarketPrice.raw);
+
+      $("#stock-div").empty();
+      $("#stock-div").append(symbolName, symbolPrice);
+
+  });
+}
+//Options 
+function getOptions(inputStock) {
+  //Yahoo Finance Api
+  var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-options?symbol=" + inputStock + "&date=1575590400",
+      "method": "GET",
+      "headers": {
+          "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+          "x-rapidapi-key": "ea9381a499msh1c1d7c0f469c737p179f05jsnd2f7d7ffd270"
+      }
+  }
+  //Sets the first box in the Table to the name of the Stock
+  $("#stock-name").text(inputStock)
+  $.ajax(settings).done(function (response) {
+      var call = response.contracts['calls'];
+      var optionsarray = Array.from(response.contracts['calls']);
+      var optionsarrayP = Array.from(response.contracts['puts']);
+
+      $("#calloptions").empty();
+      $("#IV").empty();
+      $("#IVC").empty();
+      $("#putoptions").empty();
+      $("#strike-price").empty();
+      $("#strike-priceP").empty();
+
+      for (var i = 0; i < optionsarray.length; i++) {
+          var td = $("<tr>").attr("id","HeatIndex"+i);
+          var addTD = $("#calloptions").add(td);
+          var addTD = $("#IV").add(td);
+          var addTD = $("#IVC").add(td);
+          var addTD = $("#putoptions").add(td);
+          var addTD = $("#strike-price").add(td);
+          var addTD = $("#strike-priceP").add(td);
+          var CallOptions = $("<h2>").text(optionsarray[i]['lastPrice']['fmt']);
+          var IVC = $("<h2>").text(optionsarray[i]['impliedVolatility']['fmt']);
+          var StrikePrice = $("<h2>").text(optionsarray[i]['strike']['fmt']);
+          $("#calloptions").append(CallOptions);
+          $("#IVC").append(IVC);
+          $("#strike-price").append(StrikePrice);
+      }
+      for (var i = 0; i < optionsarrayP.length; i++) {
+          var td = $("<tr>").attr("id","HeadIndex"+i );
+          var addTD = $("#IV").add(td);
+          var addTD = $("#putoptions").add(td);
+          var addTD = $("#strike-priceP").add(td);
+          console.log(optionsarrayP.length)
+          var IV = $("<h2>").text(optionsarrayP[i]['impliedVolatility']['fmt']);
+          console.log(optionsarrayP[i])
+          var PutOptions = $("<h2>").text(optionsarrayP[i]['lastPrice']['raw']);
+          var StrikePriceP = $("<h2>").text(optionsarrayP[i]['strike']['fmt']);
+          $("#IV").append(IV);
+          $("#putoptions").append(PutOptions);
+          $("#strike-priceP").append(StrikePriceP);
+      }
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
+  $.ajax(settings).done(function (response) {
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
+      //Creates an array based of the list of option calls
+
+      var optionsarray = Array.from(response.contracts['puts']);
+
+      for (var i = 0; i < optionsarray.length; i++) {
+          var iOld = i;
+          if (i - 1 > 0) {
+              iOld = i - 1
+          } else {
+              iOld = i
+          }
+          console.log(i)
+          console.log(optionsarray[i]['openInterest']);
+          var OI = (optionsarray[i]['openInterest']['fmt']);
+          var OIold = (optionsarray[iOld]['openInterest']['fmt']);
+          var NewArray = Array.from($("#putoptions"))
+          console.log(NewArray)
+          if (OI < OIold) {
+              console.log("Works")
+            
+              NewArray[i].addClass("RedHeat")
+          } else {
+             
+              NewArray[i].addClass("GreenHeat")
+          }
+      }
+
   });
-};
+}
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
