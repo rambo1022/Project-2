02734203ca61814bec74c293f8f01c4b301cf7ac
silmasis <silmasis@gmail.com>
2020-02-09 16:49:30 -0500

    fresh npm install

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,12 +22,42 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  
+  // Load index page
+  app.get("/home", function(req, res) {
+    res.render("index", {
+      title: "Home Page"
+    });
+  });
+
+  // Load tracker page
+  app.get("/tracker", function(req, res) {
+    res.render("tracker", {
+      title: "Tracker"
+    });
+  });
+
+  // Load heat map page
+  app.get("/heatmap", function(req, res) {
+    res.render("heatMap", {
+      title: "Heat Map"
+    });
+  });
+
+  // Load contact us page
+  app.get("/contactus", function(req, res) {
+    res.render("contactUs", {
+      title: "Contact Us"
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });  
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-
 };
